a73e81ba1a09f9f4b69c74773689fdcbed494c0f
Fitrianamariska <fitriana.mariska26@gmail.com>
2023-07-18 17:10:30 +0700

    sign in Merge pull request #42 from Fitrianamariska

---
+++ lib/Screens/sign_in.dart
@@ -0,0 +1,114 @@
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/widgets/placeholder.dart';
+
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'package:login_fiks2/screens/sign_up.dart';
+import 'package:login_fiks2/widget/widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignIn extends StatefulWidget {
+  //sigin homenya
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        // width: MediaQuery.of(context).size.width,
+        // height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/logo.png"),
+                SizedBox(
+                  height: 30,
+                ),
+                MyTextField("Enter Email", Icons.person_outline, false,
+                    _emailTextController),
+                SizedBox(
+                  height: 20,
+                ),
+                MyTextField("Enter Password", Icons.lock_outline, true,
+                    _passwordTextController),
+                SizedBox(
+                  height: 20,
+                ),
+                signInSignUpButton(context, true, () {
+                  FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: _emailTextController.text,
+                          password: _passwordTextController.text)
+                      .then((value) {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MyHomePage()));
+                  }).onError((error, stackTrace) {
+                    print("Error ${error.toString()}");
+                  });
+                }),
+                signUpOption()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have account?",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SignUp()));
+          },
+          child: const Text(
+            " Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+// Widget forgetPassword(BuildContext context) {
+//   return Container(
+//     width: MediaQuery.of(context).size.width,
+//     height: 35,
+//     alignment: Alignment.bottomRight,
+//     child: TextButton(
+//       child: const Text(
+//         "Forgot Password?",
+//         style: TextStyle(color: Colors.white70),
+//         textAlign: TextAlign.right,
+//       ),
+//       onPressed: () => Navigator.push(
+//           context, MaterialPageRoute(builder: (context) => ResetPassword())),
+//     ),
+//   );
+// }
