a59b45f69d4b72d99db07460cd0ad5ae51426d1d
Fitrianamariska <fitriana.mariska26@gmail.com>
2023-07-18 16:59:06 +0700

    home screen betul Merge pull request #42 from Fitrianamariska

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,134 @@
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+// import 'package:login_fiks2/screens/sign_in.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> tiles = ['CV 1', 'CV 2'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(56.0),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            AppBar(
+              backgroundColor: Colors.transparent,
+              title: Text('Recents'),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50.0),
+              topRight: Radius.circular(50.0),
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: 80.0,
+              right: 20.0,
+              left: 20.0,
+              bottom: 10.0,
+            ),
+            child: ListView.builder(
+              padding: EdgeInsets.all(8),
+              itemCount: tiles.length,
+              itemBuilder: (context, index) {
+                final tile = tiles[index];
+                return Container(
+                  margin: EdgeInsets.only(bottom: 10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      tile,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            setState(() {
+                              tiles.removeAt(index);
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () {
+                            // Aksi saat tombol edit ditekan
+                            print('Tombol edit ditekan pada $tile');
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: Center(
+  //       child: ElevatedButton(
+  //         child: Text("Logout"),
+  //         onPressed: () {
+  //           FirebaseAuth.instance.signOut().then((value) {
+  //             print("Signed Out");
+  //             Navigator.push(
+  //                 context, MaterialPageRoute(builder: (context) => SignIn()));
+  //           });
+  //           Navigator.push(
+  //               context, MaterialPageRoute(builder: (context) => SignIn()));
+  //         },
+  //       ),
+  //     ),
+  //   );
+  // }
+}
